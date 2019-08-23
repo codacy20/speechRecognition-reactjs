279f8d117450692461cdba6a1752fb42cd05597b
codacy20 <kmuc72@gmail.com>
2019-08-23 10:56:12 +0200

    finished the basic speech recognition using the npm lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import SpeechRecognition from "react-speech-recognition";
+import "./App.css";
 
 const propTypes = {
   // Props injected by SpeechRecognition
   transcript: PropTypes.string,
   resetTranscript: PropTypes.func,
-  browserSupportsSpeechRecognition: PropTypes.bool
+  browserSupportsSpeechRecognition: PropTypes.bool,
+  recognition: PropTypes.object
 };
 
-const Dictaphone = ({ transcript, resetTranscript, browserSupportsSpeechRecognition }) => {
+const Dictaphone = ({ transcript, resetTranscript, browserSupportsSpeechRecognition, recognition }) => {
   if (!browserSupportsSpeechRecognition) {
     return null;
+  } else {
+    recognition.lang = "en-US";
+    // recognition.lang = "fa-IR";
+    // recognition.lang = "en-GB";
   }
 
+  // Doesn't work at the moment
+  // if (transcript.includes("switch to Persian")) {
+  //   recognition.lang = "fa-IR";
+  //   console.log(recognition.lang);
+  // }
+
   return (
-    <div>
+    <div className="container">
       <button onClick={resetTranscript}>Reset</button>
-      <span>{transcript}</span>
+      <p>
+        <span>{transcript}</span>
+      </p>
     </div>
   );
 };
